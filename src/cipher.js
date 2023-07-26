6be961d4e242adf096a0dde6d2dc840c60240579
Alpheratzy <alexandraronvera@gmail.com>
2023-07-26 13:13:17 -0400

    fix:segundo spring

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,32 @@
 const cipher = {
-  // ...
-};
 
+  encode: function (offset, encriptar){
+  if {(encriptar && encriptar.length > 0)
+    let texto_encriptado = ""; //creación de una variable vacía para llenarla luego
+    for (let index = 0; index < encriptar.length; index++) {//vamos a recorrer el string
+      let codigoEnAscii = encriptar.charCodeAt(index)
+      if (codigoEnAscii >= 65 && codigoEnAscii <= 90) {
+        codigoEnAscii = (codigoEnAscii + offset) % 26;
+      }
+      texto_encriptado += String.fromCharCode(codigoEnAscii);
+  }
+  return texto_encriptado;
+  }
+}
+}
+
+
+/*Pasos para encode:
+1. Traer los ingredientes: numero de offset, y cadena a encriptar.
+2. Verificar  que la cadena no esté vacía.
+3. hacer un for que recorra la cadena y almacene los valores de cada letra por su número de código ASCII en una nueva variable modificable
+4. a esta nueva variable, sumarle el numero de offset. (Para que su valor cambie)
+5. Devolver el resultado a una una string
+6.imprimir esa nueva string. 
+*/
+
+  decode: function (offset, desencriptar){
+
+  }
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,73 @@
-import cipher from './cipher.js';
+/*import { clean } from 'gh-pages';
+import cipher from './cipher.js';*/
 
-console.log(cipher);
+///obj. boton borrar campos.
+
+const btn_borrar = document.getElementById("Limpiar");
+btn_borrar.addEventListener("click", () =>{
+  document.getElementById("tx_encriptable").value = "";
+  document.getElementById("tx_desencriptable").value = "";
+})
+
+//Otras constantes que necesitaré
+
+const por_encriptar = document.getElementById ("tx_encriptable");
+const por_desencriptar= document.getElementById ("tx_desencriptable");
+const numero =document.querySelector ("select");
+const btn_encriptar = document.getElementById ("encriptar");
+const btn_desencriptar= document.getElementById ("desencriptar");
+let offset=5;
+
+// funciones encode y decode pruebas
+function eleccionNumero(){
+  offset = parseInt(numero.value);//por alguna razón, me identificó el valor de mi selector como cadena, y no como número, asi que tocó parsear.
+  alert("Seleccionaste el número " + offset);
+}
+//Funciones de captura de texto:
+
+function cap_encriptar() {
+  let encriptar = por_encriptar.value;
+  encriptar = encriptar.toUpperCase();
+  //console.log (encriptar);
+  let texto_encriptado = ""; //creación de una variable vacía para llenarla luego
+  for (let index = 0; index < encriptar.length; index++) {//vamos a recorrer el string
+    let codigoEnAscii = encriptar.charCodeAt(index)
+    if (codigoEnAscii >= 65 && codigoEnAscii <= 90) { //este if, evita problemas con los carácteres especiales
+      codigoEnAscii = ((codigoEnAscii - 65 + offset) % 26) + 65;
+    }
+    texto_encriptado += String.fromCharCode(codigoEnAscii);
+  }
+  document.getElementById("tx_encriptable").value = texto_encriptado;
+  //cipher.encode();
+}
+
+function cap_desencriptar(){
+  let desencriptar = por_desencriptar.value;
+  desencriptar  = desencriptar.toUpperCase();
+ // console.log (desencriptar)
+  let texto_desencriptado = ""; //creación de una variable vacía para llenarla luego
+  for (let index = 0; index < desencriptar.length; index++) {//vamos a recorrer el string. Empieza en cero, mantiene el bucle hasta que deja de ser menor que el largo de la cadena, actualiza el bucle en una posicion del index despues de cada iteracion
+    let codigoEnAscii2 = desencriptar.charCodeAt(index) //declaro variable donde se guardará el codigo ascii, y le doy el valor a traves de pasarle el método charcodeat a mi string (letra por letra)
+    if (codigoEnAscii2 >= 65 && codigoEnAscii2 <= 90) { //porsia no haya otro tipo de caracteres en el sting. Sin embargo, lo probé sin eso, y me quitaba los acentos :S
+      codigoEnAscii2 = ((codigoEnAscii2 - 65 - offset) % 26) + 65;
+    }
+    texto_desencriptado += String.fromCharCode(codigoEnAscii2);
+  }
+  document.getElementById("tx_desencriptable").value = texto_desencriptado;
+  //cipher.decode();
+}
+
+//Eventos de botones
+
+btn_encriptar.addEventListener("click", cap_encriptar)
+//btn_encriptar.addEventListener("click", encode)
+
+btn_desencriptar.addEventListener("click", cap_desencriptar)
+
+numero.addEventListener("change", eleccionNumero);
+
+//ideas descartadas
+
+//const abcdario =["A","B","C","D","E","F","G","H","I","J","K","M","L","N","O","P","Q","R","S","T","V","W","X","Y","Z"," "];
+//let array_encriptar = Array.from(encriptar);
+
